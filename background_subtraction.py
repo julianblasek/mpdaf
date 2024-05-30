b6c26862d98b9b0c640abae555abd99e5ea61624
Julian <136075237+julianblasek@users.noreply.github.com>
2024-05-30 12:24:53 -0400

    Add background subtraction

---
+++ background_subtraction.py
@@ -0,0 +1,32 @@
+import mpdaf
+from mpdaf import obj
+from mpdaf.obj import Cube
+import numpy as np
+from mpdaf.obj import iter_spe
+
+cube=Cube("PathToCube")
+
+def substract(cube,name):
+    cont = cube.clone(data_init=np.empty, var_init=np.zeros)
+    i=0
+    shape=cube.shape
+    step=int(shape[1]*shape[2]/10)
+    print("Backgroundsubtraction:\n")
+    #Anzeige in Prozent
+    z=0
+    for sp, co in zip(iter_spe(cube), iter_spe(cont)):
+        i=i+1
+        if sp.data.max() > 0:
+            co[:] = sp.poly_spec(5)
+        else:
+            co[:] = None 
+        
+        #Anzeige in Prozent
+        if (i%step)==0:
+            z+=10
+            print(z,"%")
+        
+    subtracted_cube = cube - cont
+    subtracted_cube.write("YourPath")
+    return
+substract(cube)
